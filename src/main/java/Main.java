567ba9ebe39548ca2b4a9833c2c767ffcdd4a17c
Marion <meurantmarion@gmail.com>
2021-01-20 11:44:19 +0100

    start primary stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
 import org.tensorflow.Tensor;
 
 import javax.imageio.ImageIO;
@@ -10,9 +17,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import javafx.application.Application;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] argv) throws IOException {
+        launch(argv);
         TFUtils utils = new TFUtils();
         BufferedImage bImage = ImageIO.read(new File("src/inception5h/tensorPics/suncokret.jpg"));
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -33,5 +42,22 @@ public class Main {
 
     }
 
-
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction((action) -> {
+            System.out.println("Hello World!");
+        });
+        ImageView image = new ImageView();
+        image.setImage(new Image(getClass().getResource("inception5h/tensorPics/jack.jpg").toExternalForm()));
+        image.setFitHeight(100);
+        image.setFitWidth(100);
+        TilePane root = new TilePane();
+        root.getChildren().add(image);
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
 }
