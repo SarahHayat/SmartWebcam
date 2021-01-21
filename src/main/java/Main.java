b1c957415be6ec32b2adc7429cd2c19828574dca
SarahHayat <saraahyt@gmail.com>
2021-01-21 19:20:56 +0100

    Add filter to the cam

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,58 @@
-import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.bytedeco.javacv.*;
 import org.bytedeco.opencv.opencv_core.IplImage;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 import org.tensorflow.Tensor;
 
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.opencv.opencv_core.IplImage;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.concurrent.Executors;
 
-import static org.bytedeco.opencv.global.opencv_core.cvFlip;
 import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.*;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.Executors;
-
-import javafx.application.Application;
 
 public class Main extends Application {
+
     private Property props = new Property();
     private final Java2DFrameConverter java2DFrameConverter = new Java2DFrameConverter();
     final int INTERVALCAM = 1000;///you may use interval
+
+    private BufferedImage originalImage;
+    private BufferedImage redImage;
+    private BufferedImage greenImage;
+    private BufferedImage blueImage;
+
+    private Object value;
 
     public static void main(String[] argv) throws IOException {
         launch(argv);
@@ -92,6 +93,16 @@ public class Main extends Application {
 
         VBox boxCamera = new VBox(5);
 
+        ObservableList<String> liste = FXCollections.observableArrayList();
+        liste.add("beagle/red");
+        liste.add("mouse/blue");
+        liste.add("daisy/green");
+
+        ComboBox comboBox = new ComboBox();
+        comboBox.setValue("Filtres");
+        comboBox.setItems(liste);
+
+
         Button buttonSave = new Button();
         buttonSave.setText("Sauvegarder");
         buttonSave.setDisable(true);
@@ -123,6 +134,15 @@ public class Main extends Application {
 
         TextField tfProba = new TextField();
 
+        Label label1 = new Label("Definition");
+        Label label2 = new Label("Pourcentage");
+
+        comboBox.setOnAction((action -> {
+            value = comboBox.getSelectionModel().getSelectedItem();
+            System.out.println("VALUEEE = " + value);
+
+        }));
+
         open.setOnAction((action -> {
             ImageView imageView = new ImageView();
             OpenCVFrameGrabber opengrabber = new OpenCVFrameGrabber(0);
@@ -146,17 +166,20 @@ public class Main extends Application {
                             //save
 //                            cvSaveImage("images" + File.separator + (i++) + "-aa.jpg", img);
                             BufferedImage bufferedImage = java2DFrameConverter.getBufferedImage(frame);
-                            imageView.setImage(frameToImage(frame));
+                            imageView.setImage(frameToImage(frame, value));
 
                             Thread.sleep(INTERVALCAM);
-                            cvSaveImage("images" + File.separator + "picture.jpg", img);
+                            bufferedImage = setFilter(value, bufferedImage);
 
-                            props.setPath(Paths.get("images/picture.jpg"));
+//                            cvSaveImage("images" + File.separator + "picture"+i +".jpg", img);
+
                             props.setBufferedImage(bufferedImage);
+                            props.setPath(Paths.get("images" + File.separator + "picture"+i +".jpg"));
+                            save(props);
                             getProperty(props);
 
                             if (props.getDescription() != null) {
-                                System.out.println(i++ + " : " + props.getDescription());
+                                System.out.println(i + " : " + props.getDescription());
                                 buttonSave.setDisable(false);
                                 buttonFolder.setDisable(false);
                                 descriptionLabel.setText(props.getDescription());
@@ -169,6 +192,7 @@ public class Main extends Application {
                                 percentLabel.setText("");
 
                             }
+                            i++;
                         }
 
 
@@ -228,15 +252,17 @@ public class Main extends Application {
 
         buttonProcess.setOnAction(event);
 
-        boxPicture.getChildren().addAll(buttonUpload, buttonFolder, buttonSave, pathLabel);
+        boxPicture.getChildren().addAll(buttonFolder, buttonSave, pathLabel);
         boxPicture.setPadding(new Insets(5, 5, 5, 50));
 
-
-        boxDescLabels.getChildren().addAll(definitionLabel, tfProba, buttonProcess);
+        boxDescLabels.getChildren().addAll( label1, definitionLabel, label2, tfProba, buttonProcess);
         boxDescLabels.setPadding(new Insets(5, 5, 5, 50));
 
-        boxProperties.getChildren().addAll(descriptionLabel, percentLabel);
+        boxProperties.getChildren().addAll(label1, descriptionLabel, label2, percentLabel);
         boxProperties.setPadding(new Insets(5, 5, 5, 50));
+
+        root.getChildren().add(buttonUpload);
+        GridPane.setConstraints(buttonUpload, 4, 5);
 
         root.getChildren().add(open);
         GridPane.setConstraints(open, 4, 4);
@@ -251,21 +277,66 @@ public class Main extends Application {
         GridPane.setConstraints(boxProperties, 0, 3);
 
         root.getChildren().add(boxCamera);
-        GridPane.setConstraints(boxCamera, 0, 2);
+
+        root.getChildren().add(comboBox);
+        GridPane.setConstraints(comboBox, 1, 0);
 
 
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
-    private WritableImage frameToImage(Frame frame) {
+    private WritableImage frameToImage(Frame frame, Object value) throws IOException {
         BufferedImage bufferedImage = java2DFrameConverter.getBufferedImage(frame);
+        bufferedImage = setFilter(value, bufferedImage);
         return SwingFXUtils.toFXImage(bufferedImage, null);
+    }
+    private BufferedImage createColorImage(BufferedImage originalImage, int mask) {
+        BufferedImage colorImage = new BufferedImage(originalImage.getWidth(),
+                originalImage.getHeight(), originalImage.getType());
+
+        for (int x = 0; x < originalImage.getWidth(); x++) {
+            for (int y = 0; y < originalImage.getHeight(); y++) {
+                int pixel = originalImage.getRGB(x, y) & mask;
+                colorImage.setRGB(x, y, pixel);
+            }
+        }
+
+        return colorImage;
+    }
+
+    public BufferedImage getOriginalImage() {
+        return originalImage;
+    }
+
+    public void setOriginalImage(BufferedImage originalImage) {
+        this.originalImage = originalImage;
+        this.redImage = createColorImage(originalImage, 0xFFFF0000);
+        this.greenImage = createColorImage(originalImage, 0xFF00FF00);
+        this.blueImage = createColorImage(originalImage, 0xFF0000FF);
+    }
+
+    public BufferedImage getRedImage() {
+        return redImage;
+    }
+
+    public BufferedImage getGreenImage() {
+        return greenImage;
+    }
+
+    public BufferedImage getBlueImage() {
+        return blueImage;
     }
 
     private static void save(Property property) throws IOException {
         try {
-            ImageIO.write(property.getBufferedImage(), "jpg", property.getFile());
+            if(property.getFile() == null){
+                ImageIO.write(property.getBufferedImage() , "jpg", property.getPath().toFile());
+            }
+            else {
+                ImageIO.write(property.getBufferedImage(), "jpg", property.getFile());
+                System.out.println("GET FILE  " + property.getFile());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -282,6 +353,7 @@ public class Main extends Application {
             imageView.setImage(image1);
             imageView.setFitHeight(300);
             imageView.setFitWidth(300);
+            imageView.setStyle("-fx-background-color: RED");
             picture.getChildren().add(imageView);
             props.setPath(file.toPath());
             props.setBufferedImage(bufferedImage);
@@ -298,6 +370,29 @@ public class Main extends Application {
         File file = fileChooser.showSaveDialog(primaryStage);
         props.setFile(file);
 
+    }
+
+    public BufferedImage setFilter(Object value, BufferedImage bufferedImage){
+        setOriginalImage(bufferedImage);
+        switch (value.toString()){
+            case "beagle/red":
+                System.out.println("APPLY RED FILTER");
+                bufferedImage = getRedImage();
+                break;
+            case "mouse/blue":
+                System.out.println("APPLY BLUE FILTER");
+                bufferedImage = getBlueImage();
+                break;
+            case "daisy/green":
+                System.out.println("APPLY GREEN FILTER");
+                bufferedImage = getGreenImage();
+                break;
+            default:
+                System.out.println("APPLY NO FILTER");
+                bufferedImage = bufferedImage;
+                break;
+        }
+        return bufferedImage;
     }
 
 }
