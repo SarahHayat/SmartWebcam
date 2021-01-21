def5931d2c5e4b088b3e59bb56a9fbf4e9f51490
SarahHayat <saraahyt@gmail.com>
2021-01-21 11:17:48 +0100

    Add percentage of picture and process button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import com.sun.javafx.logging.PlatformLogger;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -7,11 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.TilePane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.tensorflow.Tensor;
@@ -21,42 +18,35 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javafx.application.Application;
 
 public class Main extends Application {
     private Property props = new Property();
+
     public static void main(String[] argv) throws IOException {
         launch(argv);
     }
+
     public static Property getProperty(Property props) throws IOException {
         TFUtils utils = new TFUtils();
-        byte [] data;
-        System.out.println("AVANT LE IF"+props.getPath());
-        if(props.getPath() != null) {
+        byte[] data;
+        if (props.getPath() != null) {
             data = Files.readAllBytes(props.getPath());
-            System.out.println("PATHHHHHHHHHHHHH"+props.getPath());
-        }else{
+        } else {
             data = Files.readAllBytes(Paths.get("src/main/resources/tensorPics/jack.jpg"));
         }
-
         String filePath = "src/inception5h/tensorflow_inception_graph.pb";
-
         List<String> labels = Files.readAllLines(Paths.get("src/inception5h/labels.txt"));
         byte[] pb = Files.readAllBytes(Paths.get(filePath));
         Tensor<Float> tensor = utils.byteBufferToTensor(data);
         Tensor<Float> result = utils.executeModelFromByteArray(pb, tensor);
-
         Description description = new Description();
         int index = description.getIndex(result, props);
         String labelText = labels.get(index);
         props.setDescription(labelText);
-        System.out.println("label = " + labelText);
         return props;
     }
 
@@ -65,39 +55,51 @@ public class Main extends Application {
         primaryStage.setTitle("Smart Webcam");
 
         GridPane root = new GridPane();
+
         Button buttonSave = new Button();
-        Button buttonUpload = new Button();
-        Button buttonFolder = new Button();
         buttonSave.setText("Sauvegarder");
-        buttonUpload.setText("Importer");
-        buttonFolder.setText("Choix du dossier");
         buttonSave.setDisable(true);
+
+        Button buttonUpload = new Button();
+        buttonUpload.setText("Importer");
+
+        Button buttonFolder = new Button();
+        buttonFolder.setText("Choix du dossier");
         buttonFolder.setDisable(true);
+
+        Button buttonProcess = new Button();
+        buttonProcess.setText("Process");
+
         TextField definition = new TextField();
 
-        Label label = new Label();
-        Label labelProba = new Label();
-        buttonFolder.setOnAction((action->{
-            FileChooser fileChooser = new FileChooser();
+        Label path = new Label();
 
-            //Set extension filter for text files
+        Label label = new Label();
+
+        Label labelProba = new Label();
+
+        TextField tfProba = new TextField();
+
+        buttonFolder.setOnAction((action -> {
+            FileChooser fileChooser = new FileChooser();
             FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.jpg");
             fileChooser.getExtensionFilters().add(extFilter);
-            System.out.println(fileChooser);
-            //Show save file dialog
             File file = fileChooser.showSaveDialog(primaryStage);
-            System.out.println("ITS FILE" + file);
-//            File initialImage = new File(extFilter + "/" + props.getPath().getFileName());
             props.setFile(file);
-
+            if (props.getFile() != null) {
+                path.setText(props.getFile().getPath());
+            }
         }));
+
         buttonSave.setOnAction((action -> {
             try {
                 save(props);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }));
+
         buttonUpload.setOnAction((action -> {
             FileChooser fileChooser = new FileChooser();
             File file = fileChooser.showOpenDialog(null);
@@ -106,46 +108,60 @@ public class Main extends Application {
                 WritableImage image1 = SwingFXUtils.toFXImage(bufferedImage, null);
                 ImageView imageView = new ImageView();
                 imageView.setImage(image1);
-                imageView.setFitHeight(100);
-                imageView.setFitWidth(100);
+                imageView.setFitHeight(300);
+                imageView.setFitWidth(300);
                 GridPane.setConstraints(imageView, 1, 0);
                 root.getChildren().add(imageView);
                 props.setPath(file.toPath());
                 props.setBufferedImage(bufferedImage);
                 getProperty(props);
-                label.setText(props.getDescription());
-                labelProba.setText(String.valueOf(props.getProba()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }));
+
         EventHandler<ActionEvent> event = (ActionEvent e) -> {
-            if(definition.getText().contains(props.getDescription())){
+            if (definition.getText().contains(props.getDescription()) && props.getProba() >= Float.parseFloat(tfProba.getText())) {
+                label.setText(props.getDescription());
+                labelProba.setText(String.valueOf(props.getProba()));
                 buttonSave.setDisable(false);
                 buttonFolder.setDisable(false);
-            }else{
+            } else {
                 buttonSave.setDisable(true);
                 buttonFolder.setDisable(true);
+                label.setText("");
+                labelProba.setText("");
             }
         };
 
-        // when enter is pressed
-        definition.setOnAction(event);
-
+        buttonProcess.setOnAction(event);
 
         root.getChildren().add(label);
-        root.getChildren().add(labelProba);
-        root.getChildren().add(definition);
-        GridPane.setConstraints(labelProba, 1, 2);
         GridPane.setConstraints(label, 1, 1);
-        GridPane.setConstraints(buttonSave, 2, 1);
-        GridPane.setConstraints(buttonUpload, 0, 0);
-        GridPane.setConstraints(buttonFolder, 0, 1);
+
+        root.getChildren().add(labelProba);
+        GridPane.setConstraints(labelProba, 1, 3);
+
+        root.getChildren().add(tfProba);
+        GridPane.setConstraints(tfProba, 1, 2);
+
+        root.getChildren().add(definition);
         GridPane.setConstraints(definition, 0, 2);
 
+        root.getChildren().add(path);
+        GridPane.setConstraints(path, 0, 3);
+
         root.getChildren().add(buttonSave);
+        GridPane.setConstraints(buttonSave, 2, 1);
+
         root.getChildren().add(buttonUpload);
+        GridPane.setConstraints(buttonUpload, 0, 0);
+
         root.getChildren().add(buttonFolder);
+        GridPane.setConstraints(buttonFolder, 0, 1);
+
+        root.getChildren().add(buttonProcess);
+        GridPane.setConstraints(buttonProcess, 0, 4);
 
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
